95374b4c3713fe2c34abb493da07cfeaa4e07bed
umedoblock <devnull@localhost>
2015-02-19 23:15:38 +0900

    refactoring. iasap.py から、iasap/__init__.py に、 argment指定の基礎となる関数を移動させた。
    (refs #393, #381)

--- py/iasap/iasap/__init__.py
+++ py/iasap/iasap/__init__.py
@@ -2,7 +2,7 @@
 
 __all__ = ['IasapCurses', 'IasapTkinter']
 
-import sqlite3, os, sys, datetime
+import sqlite3, os, sys, datetime, argparse
 import logging as _logger
 
 global logger
@@ -41,6 +41,105 @@ def start_logger(script_name, log_dir=os.path.curdir, log_level=logger.INFO):
     logger.info(horizontal)
     logger.info(body + tail)
     logger.info(horizontal)
+
+def _merge_kv(first, second, third={}):
+    kv = {}
+    for k, v in first.items():
+        kv[k] = v
+    for k, v in second.items():
+        kv[k] = v
+    for k, v in third.items():
+        kv[k] = v
+    logger.debug("kv in _merge_kv() =")
+    logger.debug(kv)
+    logger.debug("\n")
+    return kv
+
+def _get_kv_by_conf(path):
+    logger.debug("path =")
+    logger.debug(path)
+    with open(path) as f:
+        config = configparser.ConfigParser()
+        config.read_file(f, 'iasap')
+    kv = {}
+    for key, value in config["iasap"].items():
+        if value:
+            kv[key] = value
+    if kv["limit"]:
+        kv["limit"] = int(kv["limit"])
+    logger.debug("kv in _get_kv_by_conf() =")
+    logger.debug(kv)
+    logger.debug("\n")
+    return kv
+
+if os.path.islink(__file__):
+    __file__ = os.path.realpath(__file__)
+__file__ = os.path.abspath(__file__)
+dirname = os.path.dirname(__file__)
+
+DEFAULTS = {
+    "conf": os.path.join(dirname, "iasap.conf"),
+    "dbpath": os.path.join(dirname, 'iasap', 'iasap.sqlite3'),
+    "mode": "tkinter",
+    "limit": 30,
+    "debug": False,
+}
+
+def _set_kv_by_defaults():
+    kv = DEFAULTS
+    logger.debug("kv in _set_kv_by_defaults() =")
+    logger.debug(kv)
+    logger.debug("\n")
+    return kv
+
+def set_kv_for_conf():
+    kv_defaults = _set_kv_by_defaults()
+    kv_argment = _get_kv_by_argument()
+    kv_tmp = _merge_kv(kv_defaults, kv_argment)
+    return kv_tmp, kv_defaults, kv_argment
+
+def set_kv_for_regular(kv_defaults, kv_argment, conf_path):
+    kv_conf = _get_kv_by_conf(conf_path)
+    kv = _merge_kv(kv_defaults, kv_conf, kv_argment)
+    return kv
+
+def _get_kv_by_argument():
+    parser = argparse.ArgumentParser(description='argment.')
+
+    parser.add_argument('--conf', metavar='f', dest='conf',
+                       default=DEFAULTS["conf"],
+                       help='iasap.conf path.')
+    parser.add_argument('--dbpath', metavar='f', dest='dbpath',
+                       default=DEFAULTS["dbpath"],
+                       help='sqlite dbpath default is {}'.format(DEFAULTS["dbpath"]))
+    parser.add_argument('--mode', metavar='s', dest='mode',
+                       default=DEFAULTS["mode"],
+                       help='iasap default mode is tkinter, mode is tkinter or curses.')
+    parser.add_argument("--limit", metavar="N", dest="limit",
+                         type=int, default=DEFAULTS["limit"],
+                         help="max number of rows as select result, default is {}".format(DEFAULTS["limit"]))
+    parser.add_argument("--debug", help="debug mode switch, default is False.", action="store_true")
+
+    args = parser.parse_args()
+    kv = {}
+    logger.debug("sys.argv =")
+    logger.debug(sys.argv)
+    logger.debug("args =")
+    logger.debug(args)
+    logger.debug("vars(args) =")
+    logger.debug(vars(args))
+#   logger.debug("args.items() =")
+#   logger.debug(args.items())
+    shell_command = "`".join(sys.argv)
+    for k, v in vars(args).items():
+        logger.debug("k={}, v={}".format(k, v))
+        if "--{}".format(k) in shell_command:
+            # user 指定
+            kv[k] = v
+    logger.debug("kv in _get_kv_by_argument() =")
+    logger.debug(kv)
+    logger.debug("\n")
+    return kv
 
 class Iasap(object):
     def __init__(self, dbpath, height):
