05682ba9c011695570dc8a95fec514d91f45b30d
umedoblock <devnull@localhost>
2015-03-02 22:10:26 +0900

    directory 構造変更時の変更漏れ。 お引っ越し〜。
    (refs #390, #397)

---
+++ py/iasap/kit/eijiro98/create_index_on_eijiro98.sql
@@ -0,0 +1,10 @@
+# Copyright 2011-2014 梅濁酒(umedoblock)
+
+/* index
+-- CREATE INDEX index_name ON table_name(field_name);
+-- CREATE INDEX index_name ON table_name(field_name COLLATE NOCASE);
+*/
+CREATE  INDEX index_head
+        ON eijiro98(head COLLATE NOCASE);
+CREATE  INDEX index_tail
+        ON eijiro98(tail COLLATE NOCASE);
