69d73c02a96e808472351fcddc6567d72609082b
umedoblock <devnull@localhost>
2015-03-09 21:27:57 +0900

    Lisence を BSD => MIT に修正。 description を README.txt から持ってきた。
    (refs #405)

--- py/iasap/setup.py
+++ py/iasap/setup.py
@@ -11,7 +11,7 @@ setup(
     author_email = "umedoblock@gmail.com",
     url='http://pypi.python.org/pypi/iasap/',
     download_url = "",
-    license='BSD License',
+    license='MIT License',
     platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
     # see
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -30,23 +30,21 @@ setup(
     ],
     long_description = """\
 interactive as soon as possible (a.k.a asap)
--------------------------------------
-迅速な回答を心がける、対話型辞書です。
-key-value store というと、分かりやすいでしょうか。
-incremental 検索機能付き辞書です。
+--------------------------------------------
+iasap は、key-value store 形式のテキスト検索を目的として開発しました。
+iasap は、incremental 検索機能を実現しています。
 
-key は、英数字を想定しています。
-value は、日本語(=utf-8で非ascii文字)を想定しています。
+iasap は、key-value store の key, value の保存先として、sqlite3 を使用していま
+す。
+iasap は、tkinter, curses, command prompt のいずれかで、対話的に検索できます。
+iasap は、検索単語に空白を入力せずに検索すると、一致検索を実行します。
+iasap は、検索単語の前に空白を入力すると前方一致検索、 検索単語の後に空白を入力
+すると後方一致検索、を実行します。
 
-想定している具体的な使い方は、英語の辞書です。
-key に 英単語を、
-value に 日本語訳を、
-入力するという想定で開発しました。
-
-開発者の私は、手元のPCで使える英語の辞書が欲しくて、
-iasap を自作することになりました。
-英辞郎をPCで辞書として使う方法について詳しくは、
-同梱の README をご覧下さい。
+開発者の私は、手元の PC で英辞郎を使いたくて、
+iasap を開発しました。
+英辞郎を PC で辞書として使う方法について詳しくは、
+同梱の README.txt をご覧下さい。
 
 このバージョンの iasap は、Python3.x で使用してください。
 """
