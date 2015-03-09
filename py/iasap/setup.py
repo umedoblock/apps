201b2c9d5bd8a07fae329aa089a0091f02f41fbc
umedoblock <devnull@localhost>
2015-03-09 23:04:43 +0900

    version 0.0.0 で pypi に登録した時の long_description では、 改行をうまくできていなかった。 long_description が、きちんと改行されるように修正した。
    (refs #405)

--- py/iasap/setup.py
+++ py/iasap/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name = "iasap",
     packages = ["iasap"],
-    version = "0.0.0",
+    version = "0.0.1",
     description = "interactive as sonn as possible",
     author = "梅濁酒(umedoblock)",
     author_email = "umedoblock@gmail.com",
@@ -31,23 +31,23 @@ setup(
         'Topic :: Education',
     ],
     long_description = """\
-interactive as soon as possible (a.k.a asap)
---------------------------------------------
-iasap は、key-value store 形式のテキスト検索を目的として開発しました。
-iasap は、incremental 検索機能を実現しています。
-
-iasap は、key-value store の key, value の保存先として、sqlite3 を使用していま
-す。
-iasap は、tkinter, curses, command prompt のいずれかで、対話的に検索できます。
-iasap は、検索単語に空白を入力せずに検索すると、一致検索を実行します。
-iasap は、検索単語の前に空白を入力すると前方一致検索、 検索単語の後に空白を入力
-すると後方一致検索、を実行します。
-
-開発者の私は、手元の PC で英辞郎を使いたくて、
-iasap を開発しました。
-英辞郎を PC で辞書として使う方法について詳しくは、
-同梱の README.txt をご覧下さい。
-
-このバージョンの iasap は、Python3.x で使用してください。
+| interactive as soon as possible (a.k.a asap)
+| --------------------------------------------
+| iasap は、key-value store 形式のテキスト検索を目的として開発しました。
+| iasap は、incremental 検索機能を実現しています。
+| 
+| iasap は、key-value store の key, value の保存先として、sqlite3 を使用していま
+| す。
+| iasap は、tkinter, curses, command prompt のいずれかで、対話的に検索できます。
+| iasap は、検索単語に空白を入力せずに検索すると、一致検索を実行します。
+| iasap は、検索単語の前に空白を入力すると前方一致検索、 検索単語の後に空白を入力
+| すると後方一致検索、を実行します。
+| 
+| 開発者の私は、手元の PC で英辞郎を使いたくて、
+| iasap を開発しました。
+| 英辞郎を PC で辞書として使う方法について詳しくは、
+| 同梱の README.txt をご覧下さい。
+| 
+| このバージョンの iasap は、Python3.x で使用してください。
 """
 )
